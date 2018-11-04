daa360b9a5439fc0e2ba89e787a1bee774cbd259
Derick <derickjac@yahoo.com>
2018-11-04 08:01:40 -0600

    Added File

---
+++ googlescraper.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+
+
+#driver = webdriver.Chrome()
+
+origin = "houston"
+depart = "boston"
+
+driver.get("https://google.com")
+search = driver.find_element_by_name('q')
+search.send_keys(origin, " to ", depart, " cheap flight")
+search.submit()
+
+pageSource = str(driver.page_source)
+bsObj = BeautifulSoup(pageSource)
+output = bsObj.find("div", {"id": "flt"})
+#output = output.find()
+output = output.find("div", {"class": "flt-results"})
+output = output.find("span", {"class": "cUntLb"})
+print(output)
