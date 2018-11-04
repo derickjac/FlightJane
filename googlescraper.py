9ac07132e0d2c529422c2c00867b3f772864e2f6
Derick <derickjac@yahoo.com>
2018-11-04 09:30:40 -0600

    googlescraper file

--- googlescraper.py
+++ googlescraper.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from bs4 import BeautifulSoup
+import re
 
 
 #driver = webdriver.Chrome()
+chromeOptions = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : "/Users/derickjacquez/Desktop"}
+chromeOptions.add_experimental_option("prefs",prefs)
+chromedriver = "/Users/derickjacquez/Desktop/chromedriver"
+driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chromeOptions)
+
 
 origin = "houston"
 depart = "boston"
@@ -20,4 +27,5 @@ output = bsObj.find("div", {"id": "flt"})
 #output = output.find()
 output = output.find("div", {"class": "flt-results"})
 output = output.find("span", {"class": "cUntLb"})
+#output = output.findall(text=True, recursive=False)
 print(output)
